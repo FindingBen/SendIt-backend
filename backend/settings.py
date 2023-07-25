cdbce271c9876125d5c19fca49107629bbf1eb0d
Benjamin <beniagic@gmail.com>
2023-07-25 08:28:27 +0200

    Change

--- backend/settings.py
+++ backend/settings.py
@@ -16,6 +16,8 @@ if os.path.isfile(dotenv_file):
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
