5444a801b170e815c93fa8ac1b58ec2f0d12c0ef
Benjamin <beniagic@gmail.com>
2023-09-07 14:06:34 +0200

    charts

--- base/utils/googleAnalytics.py
+++ base/utils/googleAnalytics.py
@@ -12,6 +12,7 @@ from google.analytics.data_v1beta.types import (
 
 def sample_run_report(property_id="400824086", record_id=None):
     page_specified = f'/message_view/{record_id}'
+    #page_specified = f'/message_view/14'
     # print("ID", record_id)
     # Using a default constructor instructs the client to use the credentials
     # specified in GOOGLE_APPLICATION_CREDENTIALS environment variable.
@@ -22,14 +23,16 @@ def sample_run_report(property_id="400824086", record_id=None):
 
     request = RunReportRequest(
         property=f"properties/{property_id}",
-        dimensions=[Dimension(name="pagePath")],
+        dimensions=[Dimension(name="pagePath"),
+                    Dimension(name='date')],
         metrics=[
+
                  Metric(name='engagementRate'),
                  Metric(name='screenPageViews'),
                  Metric(name='userEngagementDuration'),
                  Metric(name='scrolledUsers'),
                  Metric(name='averageSessionDuration')],
-        date_ranges=[DateRange(start_date="2020-03-31", end_date="today")],
+        date_ranges=[DateRange(start_date="2020-07-31", end_date="today")],
         dimension_filter=FilterExpression(
             filter=Filter(
                 field_name="pagePath",
@@ -40,8 +43,9 @@ def sample_run_report(property_id="400824086", record_id=None):
     response = client.run_report(request)
 
     print("Report result:")
+    print(response)
     for row in response.rows:
-        print(row.dimension_values[0].value,
+        print(row.dimension_values[0].value, row.dimension_values[1].value,
               row.metric_values[0].value, row.metric_values[1].value)
 
     return response
--- base/views.py
+++ base/views.py
@@ -259,10 +259,11 @@ def get_analytics_data(request, record_id):
 
     # Extract rows from the response
     rows = analytics_data.rows
-    print(rows)
+
     formatted_data = []
     for row in rows:
-        dimension_value = row.dimension_values[0].value
+        # dimension_value = row.dimension_values[0].value
+        dimension_value = row.dimension_values[1].value
         metric_value_engegement = row.metric_values[0].value
         screen_views = row.metric_values[1].value
         user_engegment_duration = row.metric_values[2].value
@@ -270,7 +271,8 @@ def get_analytics_data(request, record_id):
         avg_session_duration = row.metric_values[4].value
         # Create a dictionary to represent the formatted row
         formatted_row = {
-            'dimension': dimension_value,
+            # 'dimension': dimension_value,
+            'date': dimension_value,
             'engegement_rate': metric_value_engegement,
             'screen_views': screen_views,
             'user_engagement_duration': user_engegment_duration,
