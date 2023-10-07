67c31203d00250590122e774a36681656290d6b2
Benjamin <beniagic@gmail.com>
2023-10-07 11:24:37 +0200

    commit

--- backend/settings.py
+++ backend/settings.py
@@ -73,7 +73,7 @@ CORS_ALLOWED_ORIGINS = [
     'https://sendit-frontend-production.up.railway.app'
 ]
 
-CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_CREDENTIALS = False
 
 CORS_ALLOW_HEADERS = ["accept",
                       "accept-encoding",
