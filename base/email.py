9d7c1fc3d6271b5ddd4f6ed95345914a24f2b9ce
Benjamin <beniagic@gmail.com>
2023-10-09 16:38:30 +0200

    commit

--- base/email.py
+++ base/email.py
@@ -5,7 +5,7 @@ class CustomPasswordResetConfirmationEmail(PasswordResetEmail):
     def get_context_data(self):
         context = super().get_context_data()
         print('TES', context)
-        context['domain'] = 'https://sendit-frontend-production.up.railway.app'
+        context['domain'] = 'sendit-frontend-production.up.railway.app'
         # context['domain'] = 'localhost:3000'
         context['protocol'] = 'https'
 
