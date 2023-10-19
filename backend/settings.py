fa4d0244a3eaba726e50878d83d5a572344829d5
FindingBen <beniagic@gmail.com>
2023-10-19 16:50:54 +0200

    commit

--- backend/settings.py
+++ backend/settings.py
@@ -68,6 +68,8 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'https://checkout.stripe.com',
+    'reverent_easley',
+    '78cfe551513a29f835306faec565114b88ebaeb1097887a8be8453cfc9b8097f'
     # Replace with the origin of your React application
     'redis://default:0FxZAn6ojRjLzCYTpXlL@containers-us-west-43.railway.app',
     'https://sendit-frontend-production.up.railway.app'
