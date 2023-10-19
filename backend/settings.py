d94f421a4b787d34f7750fd51396f6ef2c08f2e4
Benjamin <beniagic@gmail.com>
2023-10-19 09:52:51 +0200

    commit

--- backend/settings.py
+++ backend/settings.py
@@ -75,16 +75,16 @@ CORS_ALLOWED_ORIGINS = [
 
 CORS_ALLOW_CREDENTIALS = True
 
-CORS_ALLOW_HEADERS = ["accept",
-                      "accept-encoding",
-                      "authorization",
-                      "content-type",
-                      "dnt",
-                      "users",
-                      "origin",
-                      "user-agent",
-                      "x-csrftoken",
-                      "x-requested-with",]
+# CORS_ALLOW_HEADERS = ["accept",
+#                       "accept-encoding",
+#                       "authorization",
+#                       "content-type",
+#                       "dnt",
+#                       "users",
+#                       "origin",
+#                       "user-agent",
+#                       "x-csrftoken",
+#                       "x-requested-with",]
 
 TEMPLATES = [
     {
