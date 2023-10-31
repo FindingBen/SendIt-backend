98bb7ea2e248b551716a5c710abb78665eaafdaa
Benjamin <beniagic@gmail.com>
2023-10-31 09:18:47 +0100

    commit

--- payments/views.py
+++ payments/views.py
@@ -100,15 +100,15 @@ def stripe_webhook(request):
     payload = request.body
     signature_header = request.META['HTTP_STRIPE_SIGNATURE']
     event = None
-    try:
-        event = stripe.Webhook.construct_event(
-            payload, signature_header, settings.STRIPE_WEBHOOK_SECRET
-        )
-        print('test')
-    except ValueError as e:
-        return HttpResponse(status=400)
-    except stripe.error.SignatureVerificationError as e:
-        return HttpResponse(status=400)
+    # try:
+    event = stripe.Webhook.construct_event(
+        payload, signature_header, settings.STRIPE_WEBHOOK_SECRET
+    )
+    print('test')
+    # except ValueError as e:
+    #     return HttpResponse(status=400)
+    # except stripe.error.SignatureVerificationError as e:
+    #     return HttpResponse(status=400)
 
     if event['type'] == 'checkout.session.completed':
         print('test1')
