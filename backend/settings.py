ece2f1bd9ba525eb2103f97ff94851c048b40ca8
Benjamin <beniagic@gmail.com>
2023-11-18 15:24:07 +0100

    commit

--- backend/settings.py
+++ backend/settings.py
@@ -19,8 +19,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
-print(DEBUG)
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
