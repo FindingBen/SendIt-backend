5e18fa1c8b2d7ce1ca4f4c49462a03bfd3ee21b7
Benjamin <beniagic@gmail.com>
2023-12-18 12:03:04 +0100

    commit

--- sms/models.py
+++ sms/models.py
@@ -48,8 +48,8 @@ class Sms(models.Model):
                 sms_model = cls.objects.get(message_id=record_id)
                 print(values)
                 sms_model.total_bounce_rate = round(
-                    values['sorted_total_data']['bounceRate'], 2)
-                sms_model.total_overall_rate = round(values['overall_perf'], 2)
+                    values['sorted_total_data']['bounceRate'])
+                sms_model.total_overall_rate = round(values['overall_perf'])
                 sms_model.total_views = values['sorted_total_data']['screen_views_total']
 
                 print(sms_model.total_views)
