bdb6eaefd6923e02d50853ebe2dac8f2b622005a
Benjamin <beniagic@gmail.com>
2023-12-18 18:44:43 +0100

    comit

--- sms/models.py
+++ sms/models.py
@@ -37,9 +37,9 @@ class Sms(models.Model):
 
     def save(self, *args, **kwargs):
         if not self.pk:
-            print('wokring?', self)
+
             self.unique_tracking_id = str(self.unique_tracking_id)[:7]
-            self.created_at = self.created_at.strftime('%Y-%m-%d')
+            # self.created_at = self.created_at.strftime('%Y-%m-%d')
             print('S')
         super().save(*args, **kwargs)
 
