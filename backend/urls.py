68390ca7a1099d9ee4ea5b85601cb7a8cf7ed5ab
Benjamin <beniagic@gmail.com>
2024-01-04 20:30:41 +0100

    commit

--- backend/urls.py
+++ backend/urls.py
@@ -12,7 +12,6 @@ urlpatterns = [
     path('stripe/', include('payments.urls'))
 ]
 
-if settings.DEBUG:
 
-    urlpatterns += static(settings.MEDIA_URL,
+urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
