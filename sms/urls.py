9857d1eb18cea2f5c8d7919934af4c7f31b0dd18
FindingBen <beniagic@gmail.com>
2024-09-15 10:17:24 +0200

    changed click

--- sms/urls.py
+++ sms/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('sms-editor/<str:id>', views.get_message),
     path('sms/<str:id>', views.get_sms),
     path('sms-send/', createSms.as_view(), name='sms-send'),
-    path('sms-send-schedule/', views.schedule_sms,name='sms-schedule'),
-    path('sms/tracking/<str:uuid>', views.track_link_click),
+    path('sms-send-schedule/', views.schedule_sms, name='sms-schedule'),
+    path('sms/newsletter/<str:uuid>', views.track_link_click),
     path('webhooks_delivery', views.vonage_webhook)
 
 ]
