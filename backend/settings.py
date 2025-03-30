66bcc53a5fe44ef97813e8a4b8c8cebf687beb75
FindingBen <beniagic@gmail.com>
2025-03-30 12:21:03 +0200

    commit

--- backend/settings.py
+++ backend/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework_simplejwt.token_blacklist',
     "corsheaders",
-    'oauth2_provider',
     'django_rest_passwordreset',
     'djoser',
     # apps
