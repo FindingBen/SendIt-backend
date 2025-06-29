4d790d44ff092df469df748139c6545ba318e01f
FindingBen <beniagic@gmail.com>
2025-06-29 21:17:05 +0200

    commit

--- base/models.py
+++ base/models.py
@@ -200,6 +200,7 @@ class Contact(models.Model):
     first_name = models.CharField(max_length=20, blank=True, null=True)
     last_name = models.CharField(max_length=20, blank=True, null=True)
     phone_number = models.BigIntegerField(blank=True, null=True)
+    is_shopify_contact = models.BooleanField(default=False)
     email = models.EmailField(blank=True, null=True)
     created_at = models.DateField(
         auto_now_add=True)
