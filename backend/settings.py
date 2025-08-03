6483c285a83165f118ac6ec3a702294891099249
FindingBen <beniagic@gmail.com>
2025-08-03 13:10:51 +0200

    commit

--- backend/settings.py
+++ backend/settings.py
@@ -272,7 +272,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # email configs
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.privateemail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
@@ -299,7 +299,7 @@ CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
 
 USE_TZ = True
 
-DEFAULT_FROM_EMAIL = 'benarmys4@gmail.com'
+DEFAULT_FROM_EMAIL = 'support@sendperplane.com'
 
 DATE_INPUT_FORMATS = ('%d-%m-%Y', '%Y-%m-%d')
 
